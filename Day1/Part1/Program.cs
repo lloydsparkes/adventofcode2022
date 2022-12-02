1885663a26432ee79189453e6a891c297d43aaf7
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2022-12-02 12:13:59 +0000

    Day1 Commit

---
+++ Day1/Part1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var inputLines = File.ReadAllLines("input.txt");
+
+int currentHighest = 0, currentCount = 0;
+
+foreach (var line in inputLines)
+{
+    if (line == String.Empty)
+    {
+        if (currentCount > currentHighest)
+        {
+            currentHighest = currentCount;
+        }
+
+        currentCount = 0;
+    }
+    else
+    {
+        currentCount += int.Parse(line);
+    }
+}
+
+Console.WriteLine("Highest Supply Count: " + currentHighest);
