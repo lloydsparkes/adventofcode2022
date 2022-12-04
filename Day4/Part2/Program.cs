87167d087b7fccfab61edaee927dfb5b3c5f259f
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2022-12-04 14:28:28 +0000

    Day4
    Day4

---
+++ Day4/Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+
+var inputLines = File.ReadAllLines("input.txt");
+
+Console.WriteLine("Result: " + inputLines.Select(ProcessLine).Sum());
+
+int ProcessLine(string line)
+{
+    var bits = line.Split(',');
+    var rangeA = Range.ProcessText(bits[0]);
+    var rangeB = Range.ProcessText(bits[1]);
+
+    return rangeA.AnyOverlap(rangeB) ? 1 : 0;
+}
+
+public record Range(int Start, int End)
+{
+
+    public bool FullyContains(Range other)
+    {
+        return Start <= other.Start && End >= other.End;
+    }
+
+    public bool AnyOverlap(Range other)
+    {
+        return (Start <= other.Start && End >= other.Start)
+               || (Start <= other.End && End >= other.End)
+               || (Start > other.Start && End <= other.End);
+    }
+    
+    public static Range ProcessText(string input)
+    {
+        var bits = input.Split('-');
+        return new Range(int.Parse(bits[0]), int.Parse(bits[1]));
+    }
+}
