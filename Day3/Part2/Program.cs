306036f90ac815228bdc0cc65c3e6afbdc1b624c
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2022-12-04 13:56:14 +0000

    Day3

---
+++ Day3/Part2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+var inputLines = File.ReadAllLines("input.txt");
+var groups = inputLines.Chunk(3);
+
+Console.WriteLine("Result: " + groups.Select(ProcessGroup).Sum());
+
+int ProcessGroup(string[] group)
+{
+    var smallestBag = group.MinBy(a => a.Length);
+    char commonItem = Char.MinValue;
+    
+    foreach (var c in smallestBag)
+    {
+        if (group.All(s => s.Contains(c)))
+        {
+            commonItem = c;
+            break;
+        }
+    }
+    
+    if (commonItem >= 'a' && commonItem <= 'z')
+    {
+        return commonItem - 'a' + 1;
+    }
+    if (commonItem >= 'A' && commonItem <= 'Z')
+    {
+        return commonItem - 'A' + 27;
+    }
+
+    throw new UnreachableException("Should never get here");
+}
+
