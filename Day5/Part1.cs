038b06a4b9ace676b4b552311e95825cedb7dc00
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2022-12-05 16:02:02 +0000

    Day5 Initial work

--- Day5/Part1.cs
+++ Day5/Part1.cs
@@ -1,28 +1,50 @@
 ﻿using Common;
 
+public class CargoHold
+{
+    public Dictionary<int, Stack<char>> Stacks { get; set; } = new();
+
+    public static CargoHold ReadInitialStacks(string[] inputLines)
+    {
+        var newCargoHold = new CargoHold();
+        
+        var indexLine = inputLines.First(s => s.StartsWith(" 1"));
+        var cargoLines = inputLines.Where(x => x.StartsWith("[")).Reverse().ToList();
+        for (int i = 0; i < indexLine.Length; i++)
+        {
+            if (indexLine[i] != ' ')
+            {
+                var stackName = int.Parse(indexLine[i].ToString());
+                var stack = new Stack<char>();
+                foreach (var line in cargoLines)
+                {
+                    if (line[i] != ' ')
+                    {
+                        stack.Push(line[i]);
+                    }
+                }
+                newCargoHold.Stacks.Add(stackName, stack);
+            }
+        }
+
+        return newCargoHold;
+    }
+}
+
+public record Instruction(int Count, int From, int To)
+{
+    public static Instruction ReadInstruction(string input)
+    {
+        var bits = input.Split(' ');
+        return new Instruction(int.Parse(bits[1]), int.Parse(bits[3]), int.Parse(bits[5]));
+    }
+}
+
 public class Part1 : IDayPartJob
 {
     public int RunPart(string[] inputLines)
     {
-        int currentHighest = 0, currentCount = 0;
-
-        foreach (var line in inputLines)
-        {
-            if (line == String.Empty)
-            {
-                if (currentCount > currentHighest)
-                {
-                    currentHighest = currentCount;
-                }
-
-                currentCount = 0;
-            }
-            else
-            {
-                currentCount += int.Parse(line);
-            }
-        }
-
-        return currentHighest;
+        var cargoLines = inputLines.TakeWhile(x => !string.IsNullOrWhiteSpace(x));
+        var instructions = inputLines.Where(x => x.StartsWith("move")).Select(Instruction.ReadInstruction).ToList();
     }
 }
--- Day5/Part2.cs
+++ Day5/Part2.cs
@@ -4,23 +4,6 @@ public class Part2 : IDayPartJob
 {
     public int RunPart(string[] inputLines)
     {
-        int currentCount = 0;
-        var elfSupplies = new List<int>();
-
-        foreach (var line in inputLines)
-        {
-            if (line == String.Empty)
-            {
-                elfSupplies.Add(currentCount);
-                currentCount = 0;
-            }
-            else
-            {
-                currentCount += int.Parse(line);
-            }
-        }
-
-        var topThree = elfSupplies.OrderDescending().Take(3);
-        return topThree.Sum();
+        
     }
 }
