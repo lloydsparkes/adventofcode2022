cc01978c819d7639cc1cb82b95cd01f8917de773
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2022-12-06 07:09:41 +0000

    Day6

--- Day6/Part1.cs
+++ Day6/Part1.cs
@@ -4,6 +4,19 @@ public class Part1 : IDayPartJob
 {
     public object RunPart(string[] inputLines)
     {
-        return null;
+        return string.Join(",", inputLines.Select(ProcessLine));
+    }
+
+    public int ProcessLine(string line)
+    {
+        for (int i = 0; i < line.Length - 4; i++)
+        {
+            var subString = line.Substring(i, 4).ToArray();
+            if (subString.Distinct().Count() == 4)
+            {
+                return i + 4;
+            }
+        }
+        return -1;
     }
 }
--- Day6/Part2.cs
+++ Day6/Part2.cs
@@ -4,6 +4,19 @@ public class Part2 : IDayPartJob
 {
     public object RunPart(string[] inputLines)
     {
-        return null;
+        return string.Join(",", inputLines.Select(ProcessLine));
+    }
+
+    public int ProcessLine(string line)
+    {
+        for (int i = 0; i < line.Length - 14; i++)
+        {
+            var subString = line.Substring(i, 14).ToArray();
+            if (subString.Distinct().Count() == 14)
+            {
+                return i + 14;
+            }
+        }
+        return -1;
     }
 }
