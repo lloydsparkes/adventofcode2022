5a707fea42bf38b9d1c6b5b0c98f04c3a2c2ebf7
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2022-12-07 07:53:39 +0000

    Day7

--- Day7/Part1.cs
+++ Day7/Part1.cs
@@ -1,9 +1,79 @@
 ﻿using Common;
 
+public class Folder
+{
+    public string Name { get; set; }
+    
+    public Folder Parent { get; set; }
+    public List<Folder> SubFolders { get; set; } = new();
+    public List<File> Files { get; set; } = new();
+
+    public int GetFolderSize()
+    {
+        return SubFolders.Sum(f => f.GetFolderSize()) + Files.Sum(f => f.Size);
+    }
+}
+
+public record File(int Size, string Name); 
+
 public class Part1 : IDayPartJob
 {
     public object RunPart(string[] inputLines)
     {
-        return null;
+        var treeRoot = BuildTree(inputLines);
+        var folderSizes = new Dictionary<Folder, int>();
+        
+        ProcessFolder(treeRoot, folderSizes);
+
+        return folderSizes.Values.Sum(s => s <= 100000 ? s : 0);
+    }
+
+    public static void ProcessFolder(Folder folder, Dictionary<Folder, int> sizes)
+    {
+        foreach (var f in folder.SubFolders)
+        {
+            sizes.Add(f, f.GetFolderSize());
+            ProcessFolder(f, sizes);
+        }
+    }
+    
+    public static Folder BuildTree(string[] inputLines)
+    {
+        Folder root = new Folder { Name = "/" };
+        Folder current = root;
+        
+        foreach (var line in inputLines)
+        {
+            var bits = line.Split(' ');
+            if (bits[0] == "$")
+            {
+                // Command
+                if (bits[1] == "cd")
+                {
+                    if (bits[2] == "/")
+                    {
+                        current = root;
+                    }
+                    else if (bits[2] == ".." && current.Parent != null)
+                    {
+                        current = current.Parent;
+                    }
+                    else
+                    {
+                        current = current.SubFolders.First(f => f.Name == bits[2]);
+                    }
+                }
+            }
+            else if(bits[0] == "dir")
+            {
+                current.SubFolders.Add(new Folder {Name = bits[1], Parent = current});
+            }
+            else
+            {
+                current.Files.Add(new File(int.Parse(bits[0]), bits[1]));
+            }
+        }
+
+        return root;
     }
 }
--- Day7/Part2.cs
+++ Day7/Part2.cs
@@ -4,6 +4,16 @@ public class Part2 : IDayPartJob
 {
     public object RunPart(string[] inputLines)
     {
-        return null;
+        var treeRoot = Part1.BuildTree(inputLines);
+        var folderSizes = new Dictionary<Folder, int>();
+        
+        Part1.ProcessFolder(treeRoot, folderSizes);
+
+        var currentFreeSpace = 70000000 - treeRoot.GetFolderSize();
+        var requiredFreeSpace = 30000000;
+
+        var differenceToFind = requiredFreeSpace - currentFreeSpace;
+
+        return folderSizes.Values.Where(v => v >= differenceToFind).Min();
     }
 }
