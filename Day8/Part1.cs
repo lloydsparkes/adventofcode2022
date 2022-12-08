608028b17db735b7da5dc3989e71b3e511ecdbb1
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2022-12-08 08:02:59 +0000

    Day 8

--- Day8/Part1.cs
+++ Day8/Part1.cs
@@ -1,9 +1,123 @@
 ﻿using Common;
 
+public record TV(int MaxHeightSoFar, bool IsThisTreeVisible);
+
 public class Part1 : IDayPartJob
 {
     public object RunPart(string[] inputLines)
     {
-        return null;
+        var grid = BuildGrid(inputLines);
+
+        int maxX = grid.GetLength(0);
+        int maxY = grid.GetLength(1);
+
+        var visibilityGrid = new TV[maxX, maxY];
+        
+        // Need to run each pass independantly
+
+        // Top Down
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int y = 0; y < maxY; y++)
+            {
+                //The Edges are always visible
+                if (x == 0 || y == 0 || x == maxX - 1 || y == maxY - 1)
+                {
+                    visibilityGrid[x, y] = new TV(grid[x, y], true);
+                    continue;
+                }
+
+                var top = visibilityGrid[x - 1, y];
+                var isVisible = grid[x, y] > top.MaxHeightSoFar;
+                visibilityGrid[x, y] = new TV(Math.Max(grid[x,y], top.MaxHeightSoFar), isVisible);
+            }
+        }
+        
+        // Left to Right
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int y = 0; y < maxY; y++)
+            {
+                //The Edges are always visible
+                if (x == 0 || y == 0 || x == maxX - 1 || y == maxY - 1)
+                {
+                    visibilityGrid[x, y] = new TV(grid[x, y], true);
+                    continue;
+                }
+
+                var left = visibilityGrid[x, y - 1];
+                var isVisible = grid[x, y] > left.MaxHeightSoFar || visibilityGrid[x, y].IsThisTreeVisible;;
+                visibilityGrid[x, y] = new TV(Math.Max(grid[x,y], left.MaxHeightSoFar), isVisible);
+            }
+        }
+
+        // Bottom Up
+        for (int x = maxX-1; x >= 0; x--)
+        {
+            for (int y = maxY-1; y >= 0; y--)
+            {
+                if (x == 0 || y == 0 || x == maxX - 1 || y == maxY - 1)
+                {
+                    // The edges are already done
+                    continue;
+                }
+
+                var bottom = visibilityGrid[x + 1, y];
+                var isVisible = grid[x, y] > bottom.MaxHeightSoFar || visibilityGrid[x, y].IsThisTreeVisible;
+                visibilityGrid[x, y] = new TV(Math.Max(grid[x,y], bottom.MaxHeightSoFar), isVisible);
+            }
+        }
+        
+        // Right to Left
+        for (int x = maxX-1; x >= 0; x--)
+        {
+            for (int y = maxY-1; y >= 0; y--)
+            {
+                if (x == 0 || y == 0 || x == maxX - 1 || y == maxY - 1)
+                {
+                    // The edges are already done
+                    continue;
+                }
+
+                var right = visibilityGrid[x, y + 1];
+                var isVisible = grid[x, y] > right.MaxHeightSoFar || visibilityGrid[x, y].IsThisTreeVisible;
+                visibilityGrid[x, y] = new TV(Math.Max(grid[x,y], right.MaxHeightSoFar), isVisible);
+            }
+        }
+
+        int count = 0;
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int y = 0; y < maxY; y++)
+            {
+                if (visibilityGrid[x, y].IsThisTreeVisible)
+                {
+                    Console.Write('V');
+                    count++;
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
+
+        return count;
+    }
+
+    public static int[,] BuildGrid(string[] inputLines)
+    {
+        var grid = new int[inputLines.Length, inputLines.First().Length];
+
+        for (int x = 0; x < inputLines.Length; x++)
+        {
+            for (int y = 0; y < inputLines[x].Length; y++)
+            {
+                grid[x, y] = int.Parse(inputLines[x][y].ToString());
+            }
+        }
+
+        return grid;
     }
 }
--- Day8/Part2.cs
+++ Day8/Part2.cs
@@ -1,9 +1,67 @@
-﻿using Common;
+﻿using System.Threading.Tasks.Dataflow;
+using Common;
 
 public class Part2 : IDayPartJob
 {
     public object RunPart(string[] inputLines)
     {
-        return null;
+        var grid = Part1.BuildGrid(inputLines);
+
+        int maxX = grid.GetLength(0);
+        int maxY = grid.GetLength(1);
+
+        var scenicGrid = new int[maxX, maxY];
+        int maxSoFar = 0;
+        
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int y = 0; y < maxY; y++)
+            {
+                scenicGrid[x, y] = CountD(grid, x, y) * CountU(grid, x, y) * CountL(grid, x, y) * CountR(grid, x, y);
+
+                if (scenicGrid[x, y] > maxSoFar) maxSoFar = scenicGrid[x, y];
+            }
+        }
+
+        return maxSoFar;
+    }
+
+    private int CountD(int[,] grid, int fromX, int fromY)
+        => Count(grid, fromX, fromY, (x, y) => (x + 1, y));
+    private int CountL(int[,] grid, int fromX, int fromY)
+        => Count(grid, fromX, fromY, (x, y) => (x, y + 1));
+    private int CountR(int[,] grid, int fromX, int fromY)
+        => Count(grid, fromX, fromY, (x, y) => (x, y - 1));
+    private int CountU(int[,] grid, int fromX, int fromY)
+        => Count(grid, fromX, fromY, (x, y) => (x - 1, y));
+    
+    private int Count(int[,] grid, int fromX, int fromY, Func<int, int, (int x, int y)> direction)
+    {
+        int count = 0;
+        var toCheck = direction(fromX, fromY);
+        while (IsValid(grid, toCheck))
+        {
+            if (grid[toCheck.x, toCheck.y] < grid[fromX, fromY])
+            {
+                count++;
+            }
+            if (grid[toCheck.x, toCheck.y] >= grid[fromX, fromY])
+            {
+                count++;
+                break;
+            }
+
+            toCheck = direction(toCheck.x, toCheck.y);
+        }
+
+        return count;
+    }
+
+    private bool IsValid(int[,] grid, (int x, int y) input)
+    {
+        return input.x >= 0 
+               && input.y >= 0 
+               && input.x < grid.GetLength(0) 
+               && input.y < grid.GetLength(1);
     }
 }
