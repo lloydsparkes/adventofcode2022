5b52d6383891d833230ad2368e9a426040e652d0
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2022-12-12 20:18:00 +0000

    Day11 Part 1 - Part2 Failing

--- Day11/Part2.cs
+++ Day11/Part2.cs
@@ -1,9 +1,151 @@
 ﻿using Common;
 
+public record Monkey2(int Id, Func<int, int> WorryOperation, int TestDivisor, int TestTrueMonkey, int TestFalseMonkey)
+{
+    public Queue<int> Items { get; } = new Queue<int>();
+    
+    public long ItemsInspected { get; set; }
+
+    public void Process(Monkey2[] others)
+    {
+        while (Items.TryDequeue(out int item))
+        {
+            ItemsInspected++;
+            var newWorry = WorryOperation(item);
+
+            if ((newWorry % TestDivisor) == 0)
+            {
+                others[TestTrueMonkey].Items.Enqueue(newWorry);
+            }
+            else
+            {
+                others[TestFalseMonkey].Items.Enqueue(newWorry);
+            }
+        }
+    }
+}
+
 public class Part2 : IDayPartJob
 {
     public object RunPart(string[] inputLines)
     {
-        return null;
+        var monkeys = ProcessLines(inputLines);
+        int roundCount = 10000;
+
+        for (int i = 0; i < roundCount; i++)
+        {
+            foreach (var m in monkeys)
+            {
+                m.Process(monkeys);
+            }
+        }
+
+        long result = 0;
+        foreach (var m in monkeys.Select(m => m.ItemsInspected).OrderDescending().Take(2))
+        {
+            if (result == 0) result = m;
+            else result *= m;
+        }
+
+        return result;
+    }
+
+    private Monkey2[] ProcessLines(string[] inputLines)
+    {
+        var monkeys = new List<Monkey2>();
+
+        int id = -1, testDivisor = -1, testTrue = -1, testFalse = -1;
+        int[] items = null;
+        Func<int, int> op = null;
+
+        foreach (var l in inputLines)
+        {
+            if (l.StartsWith("Monkey"))
+            {
+                id = int.Parse(l[7].ToString());
+            }
+
+            if (l.Contains("Starting items:"))
+            {
+                items = l.Split(":")[1].Split(",").Select(s => s.Trim()).Select(s => int.Parse(s)).ToArray();
+            }
+
+            if (l.Contains("Operation:"))
+            {
+                var log = l.Split("=")[1].Split(" ").Select(s => s.Trim()).Skip(1).ToArray();
+
+                if (int.TryParse(log[2], out int constant))
+                {
+                    switch (log[1][0])
+                    {
+                        case '-':
+                            op = i => i - constant;
+                            break;
+                        case '+': 
+                            op = i => i + constant;
+                            break;
+                        case '*':
+                            op = i => i * constant;
+                            break;
+                        case '/':
+                            op = i => i / constant;
+                            break;
+                    }                    
+                }
+                else
+                {
+                    switch (log[1][0])
+                    {
+                        case '-':
+                            op = i => i - i;
+                            break;
+                        case '+': 
+                            op = i => i + i;
+                            break;
+                        case '*':
+                            op = i => i * i;
+                            break;
+                        case '/':
+                            op = i => i / i;
+                            break;
+                    }
+                }
+                
+            }
+
+            if (l.Contains("Test:"))
+            {
+                testDivisor = int.Parse(l.Split(" ").Last());
+            }
+            
+            if (l.Contains("If true:"))
+            {
+                testTrue = int.Parse(l.Split(" ").Last());
+            }
+            
+            if (l.Contains("If false:"))
+            {
+                testFalse = int.Parse(l.Split(" ").Last());
+            }
+
+            if (string.IsNullOrEmpty(l))
+            {
+                monkeys.Add(new Monkey2(id, op, testDivisor, testTrue, testFalse));
+
+                foreach (var item in items)
+                {
+                    monkeys.Last().Items.Enqueue(item);
+                }
+            }
+        }
+        
+        monkeys.Add(new Monkey2(id, op, testDivisor, testTrue, testFalse));
+
+        foreach (var item in items)
+        {
+            monkeys.Last().Items.Enqueue(item);
+        }
+        
+        return monkeys.ToArray();
     }
 }
