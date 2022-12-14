5dbfa3f8e072f3c0a146bb17058a8b50986263a6
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2022-12-14 06:49:58 +0000

    Day12 WIP

--- Day12/Part1.cs
+++ Day12/Part1.cs
@@ -1,9 +1,26 @@
 ﻿using Common;
 
+public record Point(int X, int Y)
+{
+    
+}
+
 public class Part1 : IDayPartJob
 {
     public object RunPart(string[] inputLines)
     {
-        return null;
+        var maxX = inputLines.First().Length;
+        var maxY = inputLines.Length;
+
+        var map = new char[maxX, maxY];
+        int y = 0;
+        foreach (var line in inputLines)
+        {
+            for (int x = 0; x < line.Length; x++)
+            {
+                map[x, y] = line[x];
+            }
+            y++;
+        }
     }
 }
